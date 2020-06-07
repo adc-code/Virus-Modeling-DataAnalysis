549f250fc50160f3921b7b854f24aa5156c4b619
adc-code <61217881+adc-code@users.noreply.github.com>
2020-06-07 14:44:37 -0400

    Add files via upload

---
+++ ON_Covid19Status_Graphs/UpdateData.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo
+echo "Getting ON Covid-19 testing data..."
+echo
+
+curl https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv > ONTCovidTesting.csv
